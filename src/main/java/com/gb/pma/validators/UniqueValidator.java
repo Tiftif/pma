d55b7b73211727dffc7a7ab80696fa251dfbde9f
[Tiftif] <gab.berteloot@gmail.com>
2020-01-15 17:08:43 +0100

    javax persistence validation mode=none

--- src/main/java/com/gb/pma/validators/UniqueValidator.java
+++ src/main/java/com/gb/pma/validators/UniqueValidator.java
@@ -21,11 +21,11 @@ public class UniqueValidator implements ConstraintValidator<UniqueValue, String>
 		Employee emp = this.empRepo.findByEmail(value);
 
 		if (emp != null) {
-
 			return false;
 		} else {
 			return true;
 		}
+
 	}
 
 }
